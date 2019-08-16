b881fda499dd3856a03ce8ba0026e70650ce2b55
Jay Chiarella <jay.gch93@gmail.com>
2019-08-16 08:08:48 +0200

    [master] Before Heroku push
    on branch [master]

---
+++ build/precache-manifest.ad035996643f8728e1aa76a662233137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abe6f95689e08a8935e5aa1e602043b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362c20e41a2f89dee8bd",
+    "url": "/static/css/main.5e636742.chunk.css"
+  },
+  {
+    "revision": "336e3e46ee57830cc08f",
+    "url": "/static/js/2.143ddbc5.chunk.js"
+  },
+  {
+    "revision": "362c20e41a2f89dee8bd",
+    "url": "/static/js/main.2a4061d3.chunk.js"
+  },
+  {
+    "revision": "e42c3f6bb30015257370",
+    "url": "/static/js/runtime~main.632a408e.js"
+  }
+]);
